b31ce20dcfc99a5f5d52c4010c8b41cd9004f4db
vikneo <vm-viktor@mail.ru>
2025-12-14 22:20:42 +0700

    add method is_valid_hash()

--- app/encrypt.py
+++ app/encrypt.py
@@ -1,9 +1,17 @@
 from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
 
 hasher = PasswordHasher()
 
 
 def hash_password(password: str) -> str:
     """Хеширование пароля"""
-
     return hasher.hash(password)
+
+
+def is_valid_hash(hash_pswd: str, password: str) -> bool:
+    try:
+        return hasher.verify(hash_pswd, password)
+    except VerifyMismatchError as err:
+        print(err)
+        return False
